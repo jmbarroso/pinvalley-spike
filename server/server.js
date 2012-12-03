f1e68ea25ded266af88e33d14daf974784aeed29
Yeray Darias <ydarias@gmail.com>
2012-12-03 20:07:37 +0000

    Versión básica del cliente sobre el recurso pon

--- server/server.js
+++ server/server.js
@@ -2,6 +2,20 @@ var express = require('express');
 
 var app = express();
 
+var allowCrossDomain = function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    res.header('Cache-Control', 'no-cache');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next();
+};
+
+app.configure(function() {
+    app.use(allowCrossDomain);
+});
+
 app.get('/hello', function(request, response) {
     var output = {
         message: "Hello world"
