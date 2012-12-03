102c4ef06832f28096f3500a0d0dcdc52e943deb
Yeray Darias <ydarias@gmail.com>
2012-12-03 19:26:12 +0000

    Servidor con petición REST para un recurso Pin

--- server/server.js
+++ server/server.js
@@ -9,4 +9,14 @@ app.get('/hello', function(request, response) {
     response.end(JSON.stringify(output));
 });
 
+app.get('/pin/:pinId', function(request, response) {
+    var pin = {
+        id: request.params.pinId,
+        name: 'Mi startup',
+        latitude: 26,
+        longitude: -17
+    };
+    response.end(JSON.stringify(pin));
+});
+
 app.listen(8080);
